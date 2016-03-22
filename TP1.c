dfcdc6e050eea4b2e7c99f37648761833b90c2ae
Sasha <kavesahares@gmail.com>
2016-03-22 15:47:58 +0300

    first comment

---
+++ TP1.c
@@ -0,0 +1,17 @@
+//
+//  TP12.c
+//  TP2_1
+//
+//  Created by Саша Яскевич on 03.03.16.
+//  Copyright © 2016 Саша Яскевич. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main() {
+    int a = 0;
+    printf("Enter your age \n");
+    scanf("%d", &a);
+    printf("Days : %d \n" , a * 365);
+    return 0;
+}
